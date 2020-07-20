bb861aa8a47af5c7a7ac11760e5d4fa4b9d170e7
VinZ889 <68568640+VinZ889@users.noreply.github.com>
2020-07-21 06:09:32 +0800

    Players Health
    There are 3 different enemy's attack that will cause damage to player's health. you need to call this health file when an enemy attack you in the main file

---
+++ PlayerHealth.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerHealth : MonoBehaviour
+{
+    [SerializeField] float arthurHealth = 3500f;
+    public float currentHealth;
+    public Slider slider;
+
+    public bool shieldActivated;
+
+    void Start()
+    {
+        currentHealth = arthurHealth;
+        slider.value = UpdateHealth();
+    }
+        
+    public void TakenDamage(float kumaDamage) // Weak Enemy Attack
+    {
+        if (shieldActivated) // where you can block the attack
+        {
+            return;
+        }
+
+        if (currentHealth > arthurHealth)
+        {
+            currentHealth = arthurHealth;
+        }
+        currentHealth -= kumaDamage;
+        slider.value = UpdateHealth();
+       
+        if (currentHealth <= 0)
+        {
+            Die();
+        } // end of Weak Enemy Attack
+        
+
+    }
+
+    public void TakenDamage1(float spiDamage) // Range Enemy attack
+    {
+        if (shieldActivated) // where you can block the attack
+        {
+            return;
+        }
+        if (currentHealth > arthurHealth)
+        {
+            currentHealth = arthurHealth;
+        }
+        currentHealth -= spiDamage;
+        slider.value = UpdateHealth();
+        
+        if (currentHealth <= 0)
+        {
+            Die();
+        }// end of Ranage Enemy Attack
+       
+    }
+
+
+    public void TakenDamage2(float trollDamage) // Strong Enemy Attack, player can not block its attack
+    {
+        if (currentHealth > arthurHealth)
+        {
+            currentHealth = arthurHealth;
+        }
+        currentHealth -= trollDamage;
+        slider.value = UpdateHealth();
+
+        if (currentHealth <= 0)
+        {
+            Die();
+        }
+        
+    }
+
+     
+    private void Die()
+    {
+        GetComponent<Animator>().SetTrigger("Dead");
+        Destroy(gameObject);
+    }
+
+    float UpdateHealth()
+    {
+        return currentHealth / arthurHealth;
+    }
+}
