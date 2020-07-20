f36fb3e0fbc1ea801fc08a973162cc17d4a8a22b
VinZ889 <68568640+VinZ889@users.noreply.github.com>
2020-07-21 06:18:21 +0800

    PlayerShield
    This needs to go into the same game object as the Player health script in order for the game. You need to call this PlayShield in your main script as well.
    Same concept with PlayerHealth

---
+++ PlayerShield.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerShield : MonoBehaviour
+{
+    private PlayerHealth health;
+    void Awake()
+    {
+        health = GetComponent<PlayerHealth>();
+    }
+
+    
+    public void ActivateShield(bool shieldActive)
+    {
+        health.shieldActivated = shieldActive;
+    }
+}
