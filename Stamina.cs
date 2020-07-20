9e32a0375c3ec5c94b16f55553c950f333397b59
VinZ889 <68568640+VinZ889@users.noreply.github.com>
2020-07-21 06:14:31 +0800

    Stamina
    Here is your stamina drop and regain file, it drops when you call an action in your main file, you need to indicate the amount of stamina drop amount.
    to regain your stamina, it is stated in this file

---
+++ Stamina.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.Profiling.Memory.Experimental;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Stamina : MonoBehaviour
+{
+    private float MaxStamina = 1000f;
+    private float currentStamina;
+    public Slider staminaBar;
+
+    private WaitForSeconds regenTick = new WaitForSeconds(0.1f);
+    private Coroutine regen;
+    public static Stamina instance;
+
+    void Awake()
+    {
+        instance = this;
+    
+    }
+
+    void Start()
+    {
+        currentStamina = MaxStamina;
+        staminaBar.maxValue = MaxStamina;
+        staminaBar.value = MaxStamina;
+    }
+
+    public void ReduceStamina(float Amount)
+    {
+        if (currentStamina - Amount >= 0)
+        {
+            currentStamina -= Amount;
+            staminaBar.value = currentStamina;
+            if (regen != null)
+                StopCoroutine(regen);
+
+                regen = StartCoroutine(RegainStamina());
+            
+        }
+        else
+            Debug.Log("Not enough stamina");
+    }
+
+    private IEnumerator RegainStamina()
+    {
+        yield return new WaitForSeconds(2);
+
+        while(currentStamina < MaxStamina)
+        {
+            currentStamina += MaxStamina / 100;
+            staminaBar.value = currentStamina;
+            yield return regenTick;
+        }
+        regen = null;
+    }
+}
